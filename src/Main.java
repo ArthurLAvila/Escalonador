c5e4efbda70fcd32414814ed494ba4ab2661b2a0
Arthur Land Avila <ArthurLAvila2000@gmail.com>
2020-09-28 18:00:44 -0300

    coloquei o nome e data

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
-// Aluno: Arthur Land Avila
+// Alunos: Arthur Land Avila & Leonardo Trindade Rubert
 // Versão: 1.0
-// Data Última Atualização: 2020 09 21
+// Data Última Atualização: 2020 09 28
 
 import java.util.PriorityQueue;
 import java.util.Queue;
